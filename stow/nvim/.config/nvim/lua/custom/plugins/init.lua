7ef2a10c12e5ec9551d08c16cd2638f81a803b03
Stuart MacCallum <maccalsa@googlemail.com>
2026-03-11 08:26:22 +0000

    add toggle term

--- stow/nvim/.config/nvim/lua/custom/plugins/init.lua
+++ stow/nvim/.config/nvim/lua/custom/plugins/init.lua
@@ -89,6 +89,73 @@ return {
       require('move').setup {}
     end,
   },
+  {
+    'akinsho/toggleterm.nvim',
+    version = '*',
+    lazy = false,
+    config = function()
+      require('toggleterm').setup {
+        size = 15,
+        open_mapping = false,
+        direction = 'float', -- Floating window (dialog-like) instead of split
+        start_in_insert = true,
+        shade_terminals = true,
+        persist_mode = true,
+        insert_mappings = true,
+        terminal_mappings = true,
+        close_on_exit = true,
+        float_opts = { border = 'single' },
+      }
+      -- Keymaps using Lua API (commands may not exist in some setups)
+      vim.keymap.set('n', '<leader>tt', function() require('toggleterm').toggle() end, { desc = 'Toggle terminal' })
+      vim.keymap.set('n', '<leader>t1', function() require('toggleterm').toggle(1) end, { desc = 'Toggle terminal 1' })
+      vim.keymap.set('n', '<leader>t2', function() require('toggleterm').toggle(2) end, { desc = 'Toggle terminal 2' })
+      vim.keymap.set('n', '<leader>t3', function() require('toggleterm').toggle(3) end, { desc = 'Toggle terminal 3' })
+      vim.keymap.set('n', '<leader>tn', function()
+        local terms = require('toggleterm.terminal').get_all(true)
+        local max_id = 0
+        for _, t in ipairs(terms) do
+          if t.id > max_id then max_id = t.id end
+        end
+        require('toggleterm').toggle(max_id + 1)
+      end, { desc = 'New terminal' })
+      vim.keymap.set('n', '<leader>ts', function()
+        local terms = require('toggleterm.terminal').get_all(true)
+        if #terms == 0 then
+          vim.notify('No terminals yet. Use <leader>tt or <leader>tn to create one.', vim.log.levels.INFO)
+          return
+        end
+        vim.ui.select(terms, {
+          prompt = 'Select terminal: ',
+          format_item = function(t) return t.id .. ': ' .. t:_display_name() end,
+        }, function(term)
+          if term then
+            if term:is_open() then term:focus() else term:open() end
+          end
+        end)
+      end, { desc = 'Select terminal' })
+      vim.keymap.set('n', '<leader>ta', function() require('toggleterm').toggle_all() end,
+        { desc = 'Toggle all terminals' })
+      vim.keymap.set('n', '<leader>tk', function()
+        if vim.bo.buftype == 'terminal' then
+          vim.cmd('bdelete!')
+        else
+          vim.notify('Not in a terminal buffer', vim.log.levels.WARN)
+        end
+      end, { desc = 'Kill current terminal' })
+      -- Exit terminal mode: Esc or Ctrl-\ Ctrl-n (built-in). Add toggleterm-specific mapping.
+      vim.api.nvim_create_autocmd('TermOpen', {
+        pattern = { 'term://*#toggleterm#*', 'term://*::toggleterm::*' },
+        callback = function()
+          vim.keymap.set('t', '<Esc>', '<C-\\><C-n>', { buffer = 0, desc = 'Exit terminal mode' })
+          vim.keymap.set('t', '<C-h>', '<Cmd>wincmd h<CR>', { buffer = 0, desc = 'Focus left window' })
+          vim.keymap.set('t', '<C-j>', '<Cmd>wincmd j<CR>', { buffer = 0, desc = 'Focus down window' })
+          vim.keymap.set('t', '<C-k>', '<Cmd>wincmd k<CR>', { buffer = 0, desc = 'Focus up window' })
+          vim.keymap.set('t', '<C-l>', '<Cmd>wincmd l<CR>', { buffer = 0, desc = 'Focus right window' })
+        end,
+      })
+    end,
+  },
   -- Alpha dashboard moved to issue_mode.lua (issue-mode layout)
   {
     -- KeyCoach: Learn Vim motions by observing your editing patterns
